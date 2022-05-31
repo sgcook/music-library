4acb4b10c68749840bbfccb46d0f2c8423b580c4
Sara Cook <saracook087@gmail.com>
2022-05-31 15:49:36 +0100

    first test is passing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const artistRouter = require('./routes/artist')
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/artist', artistRouter);
 
 module.exports = app;
