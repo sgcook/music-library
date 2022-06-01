1d83793490a084d6d62e26e675edfb2a7279982b
Sara Cook <saracook087@gmail.com>
2022-06-01 14:20:02 +0100

    can patch Artist table

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -29,3 +29,41 @@ exports.read = async (_, res) => {
 
   db.close();
 };
+
+exports.readById = async (req, res) => {
+  const db = await getDb();
+  const { artistId } = req.params;
+
+  const [[artist]] = await db.query('SELECT * FROM Artist WHERE id = ? ', [
+    artistId,
+  ]);
+
+  if (!artist) {
+    res.sendStatus(404);
+  } else {
+    res.status(200).json(artist);
+  }
+
+  db.close();
+};
+
+exports.update = async(req, res) => {
+  const db = await getDb();
+  const { artistId } = req.params;
+  const data = req.body;
+
+  try {
+    const [ { affectedRows }, ] = await db.query('UPDATE Artist SET ? WHERE id = ?', [
+      data, artistId]);
+
+    if (!affectedRows) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).send();
+    }
+  } catch(err) {
+    res.sendStatus(404);
+  }
+
+  db.close();
+};
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post('/', artistController.create);
 router.get('/', artistController.read);
+router.get('/:artistId', artistController.readById);
+router.patch('/:artistId', artistController.update);
 
 module.exports = router;
