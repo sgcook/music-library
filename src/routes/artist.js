421a2c8e165804c005a70f5bb0c955628de68ff8
Sara Cook <saracook087@gmail.com>
2022-06-06 15:42:10 +0100

    fixed routes

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const artistController = require ('../controllers/artist');
+const albumController = require('../controllers/album');
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.get('/', artistController.read);
 router.get('/:artistId', artistController.readById);
 router.patch('/:artistId', artistController.update);
 router.delete('/:artistId', artistController.destroy);
+
+router.post('/:artistId/album', albumController.create);
 
 module.exports = router;
