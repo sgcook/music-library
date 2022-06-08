2942961628dda27a564bd90dabc6ea4199516b5d
Sara Cook <saracook087@gmail.com>
2022-06-08 21:22:31 +0100

    linked artistId column

--- src/controllers/albums.js
+++ src/controllers/albums.js
@@ -4,13 +4,15 @@ const getDb = require('../services/db');
 exports.create = async (req, res) => {
   const db = await getDb();
   const { name, year } = req.body;
+  const { artistId } = req.params;
 
   try {
     await db.query(`
-    INSERT INTO Albums (name, year) 
-    VALUES (?, ?)`, [
+    INSERT INTO Albums (name, year, artistId) 
+    VALUES (?, ?, ?)`, [
       name,
       year,
+      artistId
     ]);
     res.sendStatus(201);
   } catch (err) {
